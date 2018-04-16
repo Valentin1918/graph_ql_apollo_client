ba8f410d5712a3729bb0f6d92054fbeafb864035
Grekulov <grekulov.valentyn@gmail.com>
2018-04-16 09:57:16 +0300

    worked changes

---
+++ build/graphQLClient.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
+
+import { Disclosure, configureStore, reducers, middlewares } from '../src';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+});
+
+const store = configureStore(reducers, middlewares);
+
+const app = document.getElementById('app');
+app.className = 'main-content flex-grid flex-grid--height-auto';
+
+
+
+render(
+  <ApolloProvider client={client} store={store}>
+    <Provider store={store}>
+      <Disclosure />
+    </Provider>
+  </ApolloProvider>,
+  app
+);
+
+{/*<Provider store={store}>*/}
+  {/*<Disclosure />*/}
+{/*</Provider>,*/}
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,44 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Input } from 'core-components/lib/controls/Input';
-import { Button } from 'core-components/lib/controls/Buttons/Button';
-import { Loader } from 'core-components/lib/components/Loader';
+import { compose, graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import UserQuery from '../graph/user.graphql';
 import AC from '../actions';
 import { ERSD } from '../reducers';
-import { $l } from '../app-config';
-import { INPUT_MAX_LENGTH } from '../constants/system';
-import { checkMobile } from '../utils';
 
-@connect(
-  state => ({
-    ersd: ERSD.getERSD(state),
-  }),
-  {
-    getAllData: AC.getAllData,
-    setEmail: AC.setEmail,
-    setEmailValidity: AC.setEmailValidity,
-    sendErsd: AC.sendErsd,
-    setExternalError: AC.setExternalError,
-    setPristine: AC.setPristine,
-    setDeviceType: AC.setDeviceType,
-  }
+
+
+//Another way to write a query:
+const UserQuery1 = gql`
+    query User($id: ID!, $alcoholic: Boolean!) {
+        users(id: $id){
+            id
+            name
+            friends(alcoholic: $alcoholic) {
+                name
+            }
+        }
+    }
+`;
+
+const mapResultsToProps = ({ data }) => {
+  if (!data.users) return { loading: true };
+
+  const { id, name, friends } = data.users;
+
+  return {
+    loading: false,
+    id,
+    name,
+    friends,
+  };
+};
+
+const mapPropsToOptions = ({ id, alcoholic }) => {
+  return {
+    variables: {
+      "id": 124,
+      "alcoholic": false
+    }
+  };
+};
+
+@compose(
+  connect(
+    state => ({
+      ersd: ERSD.getERSD(state),
+    }),
+    {
+      getAllData: AC.getAllData,
+    }
+  ),
+  graphql(UserQuery1, {
+    props: mapResultsToProps,
+    options: mapPropsToOptions,
+  })
 )
-export default class Authentication extends React.Component {
+export default class Authentication extends Component {
   static propTypes = {
     ersd: PropTypes.object,
     getAllData: PropTypes.func,
-    setEmail: PropTypes.func,
-    setEmailValidity: PropTypes.func,
-    sendErsd: PropTypes.func,
-    setExternalError: PropTypes.func,
-    setPristine: PropTypes.func,
-    setDeviceType: PropTypes.func,
   };
 
 
   render() {
-    console.log('render');
+    const { ersd, loading, ...matchSummaryProps } = this.props;
+    console.log('render', ersd, loading, matchSummaryProps);
     return (
       <div className="doc-collab-workflow-settings doc-collab-workflow-settings--ersd">
 
